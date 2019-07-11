93d39321243555391e970007d12613e2573e89e2
Luke Menard <lukemenard@gmail.com>
2019-07-11 17:11:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,10 +16,10 @@ def nyc_pigeon_organizer(data)
     output[number] = Hash.new {
       |key, value| key[value] = []
     }
-    data.each do |char, items|
-      output[name][char]
-      items.each do |x, array|
-        array.each do |bird_name|
-          output[bird)name][char] << feature.to_s if name == nombre
+    data.each do |attribute, items|
+      pigeon_hash[nombre][attribute]
+      items.each do |feature, arr|
+        arr.each do |name|
+          pigeon_hash[name][attribute] << feature.to_s if name == nombre
   end
 end
