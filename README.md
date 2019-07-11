838c0cd7ce2f9ccc3a20fd1fb2f728b7fcd586a0
Luke Menard <lukemenard@gmail.com>
2019-07-11 17:41:02 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## NYC Pigeon Organizer
+um## NYC Pigeon Organizer
 
 ## Learning Goals
 
--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names = []
+  output = {}
+  
+  data.each do |char, items|
+    items.each do |x, array|
+      array.each do |name|
+        if names.include?(name) == false
+          names << name
+        end
+      end
+    end
+  end
+  
+  names.each do |number|
+    output[number] = Hash.new {
+      |key, value| key[value] = []
+    }
+    data.each do |char, items|
+      output[number][char]
+      items.each do |x, array|
+        array.each do |name|
+          if name == number
+            output[name][char] << x.to_s
+          end
+        end
+      end
+    end
+  end
+  output
 end
